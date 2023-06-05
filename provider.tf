c59fc446e4bca14b6a833a508fdbbf127ca7e6ee
Supriya Ghorai <supriyagro2001@gmail.com>
2023-06-05 18:49:37 +0530

    chenges

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "demo.springboot.simple-tf-remote-state"
+    bucket = "demo-project.springboot.simple-tf-remote-state"
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
